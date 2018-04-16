b325aa5650ac2ae3988ffab92c03e4c86aac5425
Aad Versteden <madnificent@gmail.com>
2018-04-16 20:01:21 +0200

    Update test cases to use the new system
    The test cases now use EbnfInterpreter.first_match, which itself
    consumes the new generator-based solution.

--- test/ebnf_parser/interpreter_test.exs
+++ test/ebnf_parser/interpreter_test.exs
@@ -6,10 +6,13 @@ defmodule InterpreterTest do
     EbnfInterpreter.t_ep( str )
   end
 
+
   def parse_and_match( rule, str, options\\%{}) do
-    rule = Parser.full_parse( rule )
-    chars = String.codepoints( str )
-    EbnfInterpreter.eagerly_match_rule( chars, %{}, rule, options )
+    case EbnfInterpreter.first_match( rule, str, options ) do
+      { left_chars, matched, match_info } ->
+        { :ok, left_chars, matched, match_info }
+      stuff -> stuff
+    end
   end
 
 end
