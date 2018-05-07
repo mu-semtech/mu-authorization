f18e8efd64612efa5d581621188227801bc42f2b
Aad Versteden <madnificent@gmail.com>
2018-05-07 14:21:38 +0200

    Custom rendering for Inspect of %Regen.Status{}
    The EBNF syntax should be included in this object so it isn't
    recalculated everywhere.  This has very interesting benifits in terms
    of calculation, but it makes debugging near-impossible with 200k lines
    output when parsing queries.  We're overriding the inspect protocol so
    it doesn't include the syntax when the syntax is a large document.

--- lib/regen/status.ex
+++ lib/regen/status.ex
@@ -1,3 +1,34 @@
 defmodule Regen.Status do
   defstruct [ {:elements, []}, {:produced_content, []}, {:syntax, :none} ]
+
+  defimpl Inspect do
+    def inspect( %Regen.Status{ elements: elts, produced_content: content, syntax: syntax }, opts ) do
+      syntax_display =
+        case syntax do
+          :none -> syntax
+          _ ->
+            syntax_key_count = Map.keys( syntax ) |> Enum.count
+            if syntax_key_count > 10  do
+              [ syntax_key_count: syntax_key_count ]
+            else
+              syntax
+            end
+        end
+
+      {:doc_group,
+       {:doc_cons,
+        {:doc_nest,
+         {:doc_cons, "%Regen.Status{",
+          {:doc_cons, {:doc_break, "", :strict},
+           {:doc_cons,
+            {:doc_cons, {:doc_cons, "elements:", {:doc_cons, " ", Inspect.inspect( elts, opts ) }}, ","},
+            {:doc_cons, {:doc_break, " ", :strict},
+             {:doc_cons,
+              {:doc_cons, {:doc_cons, "produced_content:", {:doc_cons, " ", Inspect.inspect( content, opts )}},
+               ","},
+              {:doc_cons, {:doc_break, " ", :strict},
+               {:doc_cons, "syntax:", {:doc_cons, " ", Inspect.inspect( syntax_display, opts )}}}}}}}}, 2, :always},
+        {:doc_cons, {:doc_break, "", :strict}, "}"}}, :self}
+    end
+  end
 end
