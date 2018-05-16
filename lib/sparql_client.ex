dc5d2bfeb5ef2d71cc3124135d23d98dae04f0a8
Aad Versteden <madnificent@gmail.com>
2018-05-16 19:11:49 +0200

    Move SparqlClient and add helpers
    Naming is now consistent with other places, with things like SPARQL
    written as Sparql in the module name.  Added some helper
    functionality.

--- lib/sparql_client.ex
+++ lib/sparql_client.ex
@@ -1,7 +1,17 @@
-defmodule SPARQLClient do
-  def query(query, endpoint) do
+defmodule SparqlClient do
+  def default_endpoint do
+    "http://localhost:8890/sparql"
+  end
+
+  def query(query, endpoint\\default_endpoint) do
     options = [recv_timeout: 50000]
     HTTPoison.post!( endpoint, ["query=" <> URI.encode_www_form(query) <> "&format=" <> URI.encode_www_form("application/sparql-results+json")], ["Content-Type": "application/x-www-form-urlencoded"], options).body |> Poison.decode!
+  end
+
+  def extract_results( parsed_response ) do
+    parsed_response
+    |> Map.get("results")
+    |> Map.get("bindings")
   end
 end
 
