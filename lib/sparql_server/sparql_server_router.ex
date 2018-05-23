fc85791cf53a2b186f127d57eee9a17badfb8bd4
Aad Versteden <madnificent@gmail.com>
2018-05-23 21:24:43 +0200

    Hooking query methods to our parser/regenerator
    This doesn't improve the interface at all, but it does ensure all
    calls go through our system.  This is a first steps to get the
    requests manipulated by our handler.

--- lib/sparql_server/sparql_server_router.ex
+++ lib/sparql_server/sparql_server_router.ex
@@ -14,7 +14,7 @@ defmodule SparqlServer.Router do
 
   # TODO these methods are still very similar, I need to spent time
   #      to get the proper abstractions out
-  post "/" do
+  post "/sparql" do
     {:ok, body_params_encoded, _} = read_body(conn)
 
     body_params = body_params_encoded |> URI.decode_query
@@ -26,7 +26,7 @@ defmodule SparqlServer.Router do
     send_resp(conn, 200, response)
   end
 
-  get "/" do
+  get "/sparql" do
     params = conn.query_string |> URI.decode_query
 
     query = params["query"]
@@ -37,9 +37,16 @@ defmodule SparqlServer.Router do
 
   match _, do: send_resp(conn, 404, "404 error not found")
 
-  # TODO for now this method does not hook into our query parser
+  # TODO for now this method does not apply our access constraints
   defp handle_query(query) do
     query
+    |> String.trim
+    |> Parser.parse_query_all
+    |> Enum.filter( &Generator.Result.full_match?/1 )
+    |> List.first
+    |> Map.get( :match_construct )
+    |> List.first
+    |> Regen.result
     |> SparqlClient.query
     |> Poison.encode!
   end
