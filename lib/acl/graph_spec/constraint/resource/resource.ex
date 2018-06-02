bff7f59dda00e047ec40f5ac801b51b2710e77d7
Aad Versteden <madnificent@gmail.com>
2018-06-02 13:00:48 +0300

    First draft of type discovery for update queries
    Update queries have a set of unknown types.  We need to know the types
    in order to insert the contents into the right graphs.  The current
    implementation searches the triplestore to detect the types of unknown
    resources.
    
    There are a large set of upgrades needed in this code.  It is
    notoriously slow at the moment, as it searches for far too much
    content.  Because we know all the content that is inserted into the
    triplestore, we could memoize the types in the triplestore and avoid
    the round-trip.  Furthermore, we detect far too many types.  Much of
    this is not necessary at all.  Lastly, there is an overly large set of
    TODOs in the code which should be addressed in due time.

--- lib/acl/graph_spec/constraint/resource/resource.ex
+++ lib/acl/graph_spec/constraint/resource/resource.ex
@@ -43,23 +43,105 @@ defmodule Resource do
     by_relation ++ by_inverse_relation
   end
 
+  defp all_resources( quads ) do
+    # Yields all resources in the supplied set of quads.  These are
+    # the resources which may be of a relevant type.
+    quads
+    |> Enum.reduce( [], fn (%Quad{ subject: sub, object: obj }, prev_resources) ->
+      resources =
+        [sub, obj]
+        |> Enum.filter( &match?( %Iri{}, &1 ) )
+      resources ++ prev_resources
+    end )
+    |> Enum.uniq_by( fn (%Iri{ iri: iri }) -> iri end )
+  end
+
+  defp resources_with_types( quads, extra_quads ) do
+    # TODO: This code should cope with resources having multiple types
+    all_quads = quads ++ extra_quads
+
+    type_defs =
+      all_quads
+      |> Enum.filter( fn (%Quad{ predicate: predicate }) -> Iri.is_a?( predicate ) end )
+      |> IO.inspect( label: "typed quads" )
+      |> Enum.filter( &match?(%Quad{ object: %Iri{} },&1) ) # be defensive
+      |> Enum.map( fn(%Quad{ subject: %Iri{ iri: iri } } = quad) -> { iri, quad } end )
+      |> IO.inspect( label: "type defs before map" )
+      |> Enum.into( %{} )
+      |> IO.inspect( label: "type map" )
+
+    resources = all_resources( quads )
+
+    { known_resources, unknown_resources } =
+      resources
+      |> Enum.reduce( {[],[]},
+        fn (%Iri{ iri: iri } = resource, {known_resources,unknown_resources}) ->
+          if Map.has_key?( type_defs, iri ) do
+            resource_quad = Map.get( type_defs, iri )
+            new_resource_tuple = { resource, Map.get( resource_quad, :object ) }
+            { [ new_resource_tuple | known_resources ], unknown_resources }
+          else
+            { known_resources, [ resource | unknown_resources ] }
+          end
+        end )
+
+    # we need to enrich the unknown_resources
+    discovered_resources = discover_resources( unknown_resources )
+
+    IO.inspect discovered_resources, label: "Discovered resources"
+
+    # yield all the discovered resources
+    # the format is
+    # [{%Iri{} = resource, %Iri{} = type}]
+    known_resources ++ discovered_resources
+  end
+
+  defp discover_resources( unknown_resources ) do
+    unknown_resources
+    |> IO.inspect( label: "unknown resources to discover" )
+    |> Enum.flat_map( fn (%Iri{ iri: resource_iri_value } = resource_iri) ->
+      # TODO: this should be based on a query that was parsed first
+      # TODO: pass the right graphs to this query.  for now, it is
+      # incorrectly assumed that triplestore will solve this problem
+      # for us.
+      # TODO: cope with resources for which there is no type in code
+      # that calls discover_resources
+
+      "SELECT DISTINCT ?type WHERE { <MY_RESOURCE> a ?type }"
+      |> Parser.parse_query_full(  )
+      |> Manipulators.SparqlQuery.replace_iri( "<MY_RESOURCE>", resource_iri_value )
+      |> Regen.result
+      |> IO.inspect( label: "query to find type for " <> resource_iri_value )
+      |> SparqlClient.query
+      |> SparqlClient.extract_results
+      |> IO.inspect( label: "results for " <> resource_iri_value )
+      |> Enum.map( fn (result) ->
+        result
+        |> Map.get( "type" )
+        |> Map.get( "value" )
+        |> (fn (value) ->
+          # TODO: wrapping of iri should be handled correctly
+          { resource_iri, Iri.from_iri_string( "<" <> value <> ">" ) }
+        end ).()
+      end )
+    end )
+  end
+
   defp find_matching_resources( %Resource{ resource_type: type }, quads, extra_quads ) do
-    # We need to detect other resources of which we don't know the
-    # types by sending another query and caching the results.
+    # TODO: alter the implementation of this method by one using
+    # resources_with_types
+
     IO.puts "Checking if quads contain resources of type #{type}"
     IO.inspect quads
 
-    ( quads ++ extra_quads )
-    |> Enum.filter( fn
-      (%Quad{ object: %Iri{ iri: iri } }) -> iri == "<" <> type <> ">"
-      (%Quad{}) -> false
-    end )
-    |> IO.inspect
-    |> Enum.filter( fn (%Quad{ predicate: %Iri{ iri: iri } } ) -> iri == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>" end )
-    |> Enum.filter( fn (quad) -> Enum.member?( quads, quad ) end ) # Only yield the results from quads
-    |> IO.inspect
-    |> Enum.map( fn (%Quad{ subject: %Iri{ iri: resource } }) -> resource end ) # map to the resource
-    |> IO.inspect
+    # TODO: wrapping of iri should be handled correctly
+    wrapped_type  = "<" <> type <> ">"
+
+    resources_with_types( quads, extra_quads )
+    |> IO.inspect( label: "Resources with types" )
+    |> Enum.filter( fn ({_, %Iri{ iri: type_iri }}) -> type_iri == wrapped_type end )
+    |> Enum.map( fn({ %Iri{ iri: iri }, _ } ) -> iri end )
+    |> IO.inspect( label: "matching resources" )
   end
 
   defp filter_by_graph( %Resource{ source_graph: graph }, quads ) do
