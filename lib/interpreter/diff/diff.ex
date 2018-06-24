70c4b97a3c7f91a218ee9db7863599cf264be447
Aad Versteden <madnificent@gmail.com>
2018-06-24 17:21:18 +0200

    Logical similarity measure for queries
    Interpreter.Diff.similarity allows to calculate the difference between
    queries.  This is a basis which will allow us to calculate templates
    for the queries.

---
+++ lib/interpreter/diff/diff.ex
@@ -0,0 +1,72 @@
+alias InterpreterTerms.SymbolMatch, as: Sym
+alias InterpreterTerms.WordMatch, as: Word
+
+defmodule Interpreter.Diff do
+  def similarity( a, b ) do
+    { matching, total } = similarity_calc( a, b )
+    matching / total
+  end
+
+  @doc """
+  Returns a similarity number.  Comparing how similar the two objects
+  are.
+
+  We compare this by looking at the amount of terms in the query, and
+  seeing how much they overlap.
+
+  The matching is returned as a tuple containing the total amount of
+  positive similarities as the first value, and the total amount of
+  compared similarities as the second value.
+
+  @return { positive_similaties, total_similarities }
+  """
+  def similarity_calc( %Sym{ submatches: asub } = a, %Sym{ submatches: bsub } = b ) do
+    if shallow_same?( a, b ) do
+      { self_positive_similarities, self_total_similarities } = { 1, 1 }
+
+      asub = if asub == :none do [] else asub end
+      bsub = if bsub == :none do [] else bsub end
+
+      longest_length = max( Enum.count( asub ), Enum.count( bsub ) )
+      shortest_length = min( Enum.count( asub ), Enum.count( bsub ) )
+
+      { sub_positive_similarities, sub_total_similarities } =
+        [ asub, bsub ]
+        |> Enum.zip
+        |> Enum.reduce( {0,0}, fn (sub_elts, acc_similarities) ->
+          {asub_elt, bsub_elt} = sub_elts
+          { positive_similarities, total_similarities } = acc_similarities
+          {add_pos, add_total} = similarity_calc( asub_elt, bsub_elt )
+          {positive_similarities + add_pos, total_similarities + add_total}
+        end )
+
+      { missing_matches_positive_similarities, missing_matches_total_similarities } =
+        { 0, longest_length - shortest_length }
+
+      { self_positive_similarities + sub_positive_similarities + missing_matches_positive_similarities,
+        self_total_similarities + sub_total_similarities + missing_matches_total_similarities }
+    else
+      { 0, 1 }
+    end
+  end
+  def similarity_calc( a, b ) do
+    if shallow_same?( a, b ) do
+      { 1, 1 }
+    else
+      { 0, 1 }
+    end
+  end
+
+  def shallow_same?( %Sym{ symbol: a, submatches: :none, string: str_a }, %Sym{ symbol: a, submatches: :none, string: str_b } ) do
+    str_a == str_b
+  end
+  def shallow_same?( %Sym{ symbol: a }, %Sym{ symbol: a } ) do
+    true
+  end
+  def shallow_same?( %Word{ word: word }, %Word{ word: word } ) do
+    true
+  end
+  def shallow_same?( _, _ ) do
+    false
+  end
+end
