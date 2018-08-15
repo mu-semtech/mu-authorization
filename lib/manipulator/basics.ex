1d5e46615b71d1d94fab9571ad819d587a16500c
Aad Versteden <madnificent@gmail.com>
2018-08-15 17:01:55 +0200

    Add a tree walker which can maintain state
    The previous tree walker could walk the tree and yield results.  The
    new walker can also keep a state which is stored whilst walking.  This
    state may be necessary when walking the tree as Elixir is purely
    functional in its functions.

--- lib/manipulator/basics.ex
+++ lib/manipulator/basics.ex
@@ -56,4 +56,77 @@ defmodule Manipulators.Basics do
     symbol
   end
 
+
+
+  @doc """
+
+  Walks the results recursively, until requested to stop.  Behaves in
+  the same way as map_matches, but remembers the state.
+
+  Note that some keywords are currently not allowed as state, as their
+  terms are used internally.  Namely :replace_by, :skip,
+  :insert_after, :exit, :continue, :replace_and_traverse.
+
+  # TODO: remove shared code with map_matches
+  """
+  def map_matches_with_state( start_state, result, functor ) do
+    case functor.( start_state, result ) do
+      # State yielding options
+      { :replace_by, new_state, content } ->
+        { new_state, content }
+      { :skip, new_state } ->
+        { new_state, result }
+      # Tree operations
+      { :insert_after, new_state, content } ->
+        { :insert_after, new_state, content }
+      { :exit, new_state, value } ->
+        { :exit, new_state, value }
+      # Dispatch to submatches
+      { :continue, new_state } ->
+        map_submatches_with_state( new_state, result, functor )
+      { :replace_and_traverse, new_state, content } ->
+        map_submatches_with_state( new_state, content, functor )
+    end
+  end
+
+  # TODO: remove shared code with map_submatches
+  def map_submatches_with_state( state, %InterpreterTerms.SymbolMatch{ submatches: submatches } = symbolmatch, functor )
+  when is_list( submatches ) do
+    case (
+      # Use reduce_while so we can exit early when requested
+      submatches
+      |> Enum.reduce_while( {state,[]},
+         fn (sub, {state,acc}) ->
+           res = map_matches_with_state( state, sub, functor )
+           case res do
+             { :insert_after, new_state, elt } ->
+               new_acc = { new_state, [[sub,elt] | acc] }
+               { :continue, new_acc }
+             { :exit, new_state, value } ->
+               { :halt, { :exit, new_state, value } }
+             { new_state, new_result } ->
+               { :cont, { new_state, [[ new_result ] | acc] } }
+           end
+         end ) )
+    do
+      # If we had to short-circuit due to exit, exit
+      { :exit, state, value } ->
+        { :exit, state, value }
+      # Otherwise use the submatches to yield the new result
+      { new_state, submatches } ->
+        new_submatches =
+          submatches
+          |> Enum.reverse
+          |> Enum.flat_map( &(&1) )
+
+      { new_state,
+        %{ symbolmatch
+           | submatches: new_submatches } }
+    end
+  end
+
+  def map_submatches_with_state( state, symbol, _ ) do
+    { state, symbol }
+  end
+
 end
