81a2d06dbac18846ac7f85949c230c11518088da
Aad Versteden <madnificent@gmail.com>
2018-10-20 11:53:01 +0200

    Simplify ..GroupGraphPattern.extract_triples_blocks
    The new macro abstraction allows us to simplify some of the code.  As
    we currently only have this abstraction available for the state_map,
    this is the only place where the change makes sense.

--- lib/graph_reasoner/query_matching/group_graph_pattern.ex
+++ lib/graph_reasoner/query_matching/group_graph_pattern.ex
@@ -3,6 +3,7 @@ alias InterpreterTerms.WordMatch, as: Word
 alias Updates.QueryAnalyzer.Iri, as: Iri
 
 defmodule GraphReasoner.QueryMatching.GroupGraphPattern do
+  require Manipulators.Basics
 
   def only_triples_blocks?( symbol ) do
     # Ensure this only consists of TriplesBlock instances
@@ -34,29 +35,21 @@ defmodule GraphReasoner.QueryMatching.GroupGraphPattern do
     # Extract each of the TriplesBlock instances
     # >> Must yield a list of TriplesBlock elements
     #    with no nested TriplesBlocks elements inside of them
-    { state, _mapped_query } =
-      Manipulators.Basics.map_matches_with_state( [], symbol, fn ( state, match ) ->
-        case match do
-          %Sym{ symbol: :TriplesBlock } ->
-            new_triples_blocks = extract_triples_blocks_from_triples_blocks( match )
-            # IO.inspect( new_triples_blocks, label: "new triples blocks" )
-            # IO.inspect( state, label: "old state" )
-
-            { :skip, new_triples_blocks ++ state }
-          _ -> { :continue, state }
-        end
-      end )
-
-    state
+    Manipulators.Basics.do_state_map( { [], symbol }, { state, match } ) do
+      :TriplesBlock ->
+        new_triples_blocks = extract_triples_blocks_from_triples_block( match )
+        { :skip, new_triples_blocks ++ state }
+    end
+    |> elem(0)
   end
 
-  defp extract_triples_blocks_from_triples_blocks( %Sym{ symbol: :TriplesBlock,
+  defp extract_triples_blocks_from_triples_block( %Sym{ symbol: :TriplesBlock,
                                                         submatches: submatches } = triples_block )  do
     case submatches do
       [ _element ] -> [triples_block]
       [ _element, _word ] -> [triples_block]
       [ element, _word, next_triples_block ] ->
-        [ %{ triples_block | submatches: [ element ] } | extract_triples_blocks_from_triples_blocks( next_triples_block ) ]
+        [ %{ triples_block | submatches: [ element ] } | extract_triples_blocks_from_triples_block( next_triples_block ) ]
     end
   end
 
