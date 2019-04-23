b46535dbc3d1972a2bd5df4fe17b195e03fcf818
Aad Versteden <madnificent@gmail.com>
2019-04-23 12:28:12 +0200

    Fix code formatting for QueryMatching.PathPrimary

--- lib/graph_reasoner/query_matching/path_primary.ex
+++ lib/graph_reasoner/query_matching/path_primary.ex
@@ -1,9 +1,7 @@
 alias InterpreterTerms.SymbolMatch, as: Sym
-alias InterpreterTerms.WordMatch, as: Word
 alias Updates.QueryAnalyzer.Iri, as: Iri
 
 defmodule GraphReasoner.QueryMatching.PathPrimary do
-
   @moduledoc """
   Helpers to make sense of a PathPrimary.
   """
@@ -12,19 +10,18 @@ defmodule GraphReasoner.QueryMatching.PathPrimary do
   Returns an Iri object for the supplied :PathPrimary, or crashes if
   it can not achieve that.
   """
-  def iri!( %Sym{ symbol: :PathPrimary,
-                  submatches: [ submatch ] } ),
-    do: Iri.from_symbol( submatch )
+  def iri!(%Sym{symbol: :PathPrimary, submatches: [submatch]}) do
+    Iri.from_symbol(submatch)
+  end
 
-  def iri!(
-    %Sym{ symbol: :PathPrimary,
-          string: str } )
-    do
-
+  def iri!(%Sym{symbol: :PathPrimary, string: str}) do
     # We also support the variant where the PathPrimary was not
     # correctly rewritten to something understood by Iri, as we
     # suspect this may occur when users rewrite the actual query.
-    Iri.from_iri_string( str )
+    Iri.from_iri_string(str)
   end
 
+  def iri!(%Sym{symbol: :PathPrimary, submatches: [submatch]}, prologue_info) do
+    Iri.from_symbol(submatch, prologue_info)
+  end
 end
