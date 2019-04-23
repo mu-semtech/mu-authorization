0f06b4df99d970dd814829099181acedc0df3dcc
Aad Versteden <madnificent@gmail.com>
2019-04-23 12:10:11 +0200

    Resolves code-formatting of MatchTypes
    As we now have new tooling, we can gain consistency by automatically
    indenting a bunch of code as desired.  This should increase
    readability over time.  We had this in a previous setup, but the code
    indentation was badly done there making it hard to rely on.

--- lib/updates/query_analyzer/match_types.ex
+++ lib/updates/query_analyzer/match_types.ex
@@ -8,18 +8,18 @@ alias InterpreterTerms.SymbolMatch, as: Sym
 alias InterpreterTerms.WordMatch, as: Word
 
 defprotocol Updates.QueryAnalyzer.P do
-  def to_solution_sym( element )
+  def to_solution_sym(element)
 
-  def to_sparql_result_value( element )
+  def to_sparql_result_value(element)
 end
-
 
 defmodule Iri do
   defstruct [:iri, :real_name]
 
-  def from_iri_string( iri, _options \\ [] ) do
-    new_iri = String.trim( iri, " " ) # TODO remove trimming when terminal symbols don't emit spaces anymore
-    %Iri{ iri: new_iri, real_name: new_iri }
+  def from_iri_string(iri, _options \\ []) do
+    # TODO remove trimming when terminal symbols don't emit spaces anymore
+    new_iri = String.trim(iri, " ")
+    %Iri{iri: new_iri, real_name: new_iri}
   end
 
   def from_prefix_string(prefixed_name, options) do
@@ -62,34 +62,46 @@ defmodule Iri do
   A best effort to convert a symbol containing an Iri into an Iri object
 
   """
-  def from_symbol( %InterpreterTerms.WordMatch{ word: "a" } ) do
-    Iri.make_a
-  end
-  def from_symbol( %InterpreterTerms.SymbolMatch{ symbol: :iri,
-                         submatches: [ %InterpreterTerms.SymbolMatch{ symbol: :IRIREF, string: str } ] } ) do
-    Iri.from_iri_string( str )
+  def from_symbol(a) do
+    from_symbol(a, %{})
   end
 
-  def wrap_iri_string( iri_string ) do
+  def from_symbol(%Word{word: "a"}, _options) do
+    Iri.make_a()
+  end
+
+  def from_symbol(%Sym{symbol: :iri, submatches: [%Sym{symbol: :IRIREF, string: str}]}, _options) do
+    Iri.from_iri_string(str)
+  end
+
+  def from_symbol(
+        %Sym{symbol: :iri, submatches: [%Sym{symbol: :PrefixedName, string: str}]},
+        options
+      ) do
+    Iri.from_prefix_string(str, options)
+  end
+
+  def wrap_iri_string(iri_string) do
     "<" <> iri_string <> ">"
   end
 
-  def unwrap_iri_string( iri_string ) do
-    String.slice( iri_string, 1, String.length( iri_string ) - 2 )
+  def unwrap_iri_string(iri_string) do
+    String.slice(iri_string, 1, String.length(iri_string) - 2)
   end
 
   def make_a do
-    %Iri{ iri: "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>", real_name: "a" }
+    %Iri{iri: "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>", real_name: "a"}
   end
 
-  def is_a?( %Iri{ iri: iri } ) do
+  def is_a?(%Iri{iri: iri}) do
     iri == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
   end
 
-  def same?( %Iri{ iri: iri }, %Iri{ iri: iri } ) do
+  def same?(%Iri{iri: iri}, %Iri{iri: iri}) do
     true
   end
-  def same?( _, _ ) do
+
+  def same?(_, _) do
     false
   end
 
@@ -100,27 +112,26 @@ defmodule Iri do
   """
   def default_prefixes do
     %{}
-    |> Map.put( "xsd", Iri.from_iri_string( "<http://www.w3.org/2001/XMLSchema#>" ) )
+    |> Map.put("xsd", Iri.from_iri_string("<http://www.w3.org/2001/XMLSchema#>"))
   end
 
-  defp strip_iri_marks( string ) do
-    String.slice( string, 1, String.length( string ) - 2 )
+  defp strip_iri_marks(string) do
+    String.slice(string, 1, String.length(string) - 2)
   end
 
   defimpl Updates.QueryAnalyzer.P do
-    def to_solution_sym( %Iri{ iri: full_name } ) do
+    def to_solution_sym(%Iri{iri: full_name}) do
       # TODO: Emit values the way we received them
       %InterpreterTerms.SymbolMatch{
         symbol: :iri,
         submatches: [
-          %InterpreterTerms.SymbolMatch{
-            symbol: :IRIREF,
-            string: full_name,
-            submatches: :none } ] }
+          %InterpreterTerms.SymbolMatch{symbol: :IRIREF, string: full_name, submatches: :none}
+        ]
+      }
     end
 
-    def to_sparql_result_value( %Iri{ iri: full_iri } ) do
-      %{ type: "uri", value: Iri.unwrap_iri_string( full_iri ) }
+    def to_sparql_result_value(%Iri{iri: full_iri}) do
+      %{type: "uri", value: Iri.unwrap_iri_string(full_iri)}
     end
   end
 end
@@ -128,15 +139,15 @@ end
 defmodule Var do
   defstruct [:full_name]
 
-  def from_string( value ) do
-    %Var{ full_name: value }
+  def from_string(value) do
+    %Var{full_name: value}
   end
 
-  def is_var( %Var{} ) do
+  def is_var(%Var{}) do
     true
   end
 
-  def is_var( _ ) do
+  def is_var(_) do
     false
   end
 
@@ -146,25 +157,26 @@ defmodule Var do
       iex> Updates.QueryAnalyzer.Variable.pure_name( %Updates.QueryAnalyzer.Variable{ full_name: "?foo" } )
       > "foo"
   """
-  def pure_name( %Var{ full_name: full_name } ) do
-    { _, pure_name } = String.next_grapheme( full_name )
+  def pure_name(%Var{full_name: full_name}) do
+    {_, pure_name} = String.next_grapheme(full_name)
     pure_name
   end
 
   defimpl Updates.QueryAnalyzer.P do
-    def to_solution_sym( %Var{} = var ) do
-      Var.to_solution_sym( var )
+    def to_solution_sym(%Var{} = var) do
+      Var.to_solution_sym(var)
     end
-    def to_sparql_result_value( %Var{} ) do
+
+    def to_sparql_result_value(%Var{}) do
       raise "Cannot convert Variable to SPARQL1.1 result"
     end
   end
 
-  def to_solution_sym( %Var{ full_name: full_name } ) do
+  def to_solution_sym(%Var{full_name: full_name}) do
     symbol =
       case full_name do
-        << "?", _ :: binary >> -> :VAR1
-        << "$", _ :: binary >> -> :VAR2
+        <<"?", _::binary>> -> :VAR1
+        <<"$", _::binary>> -> :VAR2
       end
 
     %InterpreterTerms.SymbolMatch{
@@ -183,42 +195,52 @@ end
 defmodule Bool do
   defstruct [:value]
 
-  def from_string( value ) do
-    bool = case String.downcase( value ) do
-             "true" -> true
-             "false" -> false
-           end
+  def from_string(value) do
+    bool =
+      case String.downcase(value) do
+        "true" -> true
+        "false" -> false
+      end
 
-    %Bool{ value: bool }
+    %Bool{value: bool}
   end
-  def from_boolean( boolean ) when is_boolean( boolean ) do
-    %Bool{ value: boolean }
+
+  def from_boolean(boolean) when is_boolean(boolean) do
+    %Bool{value: boolean}
   end
 
   defimpl Updates.QueryAnalyzer.P do
-    def to_solution_sym( %Bool{ value: value } ) do
-      word = if value do
-        "true"
-      else
-        "false"
-      end
+    def to_solution_sym(%Bool{value: value}) do
+      word =
+        if value do
+          "true"
+        else
+          "false"
+        end
 
       %InterpreterTerms.SymbolMatch{
         symbol: :RDFLiteral,
         submatches: [
           %InterpreterTerms.SymbolMatch{
             symbol: :BooleanLiteral,
-            submatches: [
-              %InterpreterTerms.WordMatch{
-                word: word } ] } ] }
+            submatches: [%InterpreterTerms.WordMatch{word: word}]
+          }
+        ]
+      }
     end
 
-    def to_sparql_result_value( %Bool{value: value} ) do
-      value_string = case value do
-                       true -> "true"
-                       false -> "false"
-                     end
-      %{ type: "literal", value: value_string, datatype: "http://www.w3.org/2001/XMLSchema#boolean" }
+    def to_sparql_result_value(%Bool{value: value}) do
+      value_string =
+        case value do
+          true -> "true"
+          false -> "false"
+        end
+
+      %{
+        type: "literal",
+        value: value_string,
+        datatype: "http://www.w3.org/2001/XMLSchema#boolean"
+      }
     end
   end
 end
@@ -226,18 +248,20 @@ end
 defmodule Str do
   defstruct [:str, {:lang, false}, {:type, false}]
 
-  def from_string( string ) do
-    %Str{ str: string }
+  def from_string(string) do
+    %Str{str: string}
   end
-  def from_langstring( string, language ) do
-    %Str{ str: string, lang: language }
+
+  def from_langstring(string, language) do
+    %Str{str: string, lang: language}
   end
-  def from_typestring( string, type ) do
-    %Str{ str: string, type: type }
+
+  def from_typestring(string, type) do
+    %Str{str: string, type: type}
   end
 
   defimpl Updates.QueryAnalyzer.P do
-    def to_solution_sym( %Str{ str: string, lang: lang, type: type } = str ) do
+    def to_solution_sym(%Str{str: string, lang: lang, type: type} = str) do
       # TODO: handle escaping of strings correctly, depending where
       # they came from.  This requires changes in from_string
       # (optional argument), and Updates.QueryAnalyzer#primitive_value_from_binding
@@ -246,19 +270,25 @@ defmodule Str do
       triple_escaped_string = string
 
       case str do
-        %Str{ lang: false, type: false } ->
+        %Str{lang: false, type: false} ->
           # it is a simple string
           %InterpreterTerms.SymbolMatch{
             symbol: :RDFLiteral,
             submatches: [
-            %InterpreterTerms.SymbolMatch{
-              symbol: :String,
-              submatches: [
-                %InterpreterTerms.SymbolMatch{
-                  symbol: :STRING_LITERAL_LONG_2,
-                  string: triple_escaped_string,
-                  submatches: :none } ] } ] }
-        %Str{ lang: false } ->
+              %InterpreterTerms.SymbolMatch{
+                symbol: :String,
+                submatches: [
+                  %InterpreterTerms.SymbolMatch{
+                    symbol: :STRING_LITERAL_LONG_2,
+                    string: triple_escaped_string,
+                    submatches: :none
+                  }
+                ]
+              }
+            ]
+          }
+
+        %Str{lang: false} ->
           # it is a typed string
           %InterpreterTerms.SymbolMatch{
             symbol: :RDFLiteral,
@@ -269,10 +299,16 @@ defmodule Str do
                   %InterpreterTerms.SymbolMatch{
                     symbol: :STRING_LITERAL_LONG_2,
                     string: triple_escaped_string,
-                    submatches: :none } ] },
-              %InterpreterTerms.WordMatch{ word: "^^" },
-              Updates.QueryAnalyzer.P.to_solution_sym( type ) ] }
-        %Str{ type: false } ->
+                    submatches: :none
+                  }
+                ]
+              },
+              %InterpreterTerms.WordMatch{word: "^^"},
+              Updates.QueryAnalyzer.P.to_solution_sym(type)
+            ]
+          }
+
+        %Str{type: false} ->
           # it is a language typed string
           %InterpreterTerms.SymbolMatch{
             symbol: :RDFLiteral,
@@ -283,41 +319,49 @@ defmodule Str do
                   %InterpreterTerms.SymbolMatch{
                     symbol: :STRING_LITERAL_LONG_2,
                     string: triple_escaped_string,
-                    submatches: :none } ] },
+                    submatches: :none
+                  }
+                ]
+              },
               %InterpreterTerms.SymbolMatch{
                 symbol: :LANGTAG,
                 string: "@" <> lang,
-                submatches: :none } ] }
+                submatches: :none
+              }
+            ]
+          }
       end
     end
 
-    def to_sparql_result_value( %Str{ str: str, lang: false, type: false } ) do
-      %{ type: "literal", value: str }
+    def to_sparql_result_value(%Str{str: str, lang: false, type: false}) do
+      %{type: "literal", value: str}
     end
-    def to_sparql_result_value( %Str{ str: str, lang: lang, type: false } ) do
+
+    def to_sparql_result_value(%Str{str: str, lang: lang, type: false}) do
       # TODO discuss whether it's really ok to write xml:lang here,
       # instead of writing out the xml namespace.
-      %{ type: "literal", value: str, "xml:lang": lang }
+      %{type: "literal", value: str, "xml:lang": lang}
     end
-    def to_sparql_result_value( %Str{ str: str, lang: false, type: %Iri{ iri: type_iri } } ) do
-      type = Iri.unwrap_iri_string( type_iri )
-      %{ type: "literal", value: str, datatype: type }
+
+    def to_sparql_result_value(%Str{str: str, lang: false, type: %Iri{iri: type_iri}}) do
+      type = Iri.unwrap_iri_string(type_iri)
+      %{type: "literal", value: str, datatype: type}
     end
   end
 end
 
 defmodule Number do
-  defstruct [ :str ]
+  defstruct [:str]
 
-  def from_string( string ) do
-    %Str{ str: string }
+  def from_string(string) do
+    %Str{str: string}
   end
 
   defimpl Updates.QueryAnalyzer.P do
-    def to_solution_sym( %Number{ str: str } ) do
+    def to_solution_sym(%Number{str: str}) do
       # TODO this does not necessarily emit the correct structure, yet
       # it will yield the correct SPARQL query.
-      
+
       %InterpreterTerms.SymbolMatch{
         symbol: :RDFLiteral,
         submatches: [
@@ -327,19 +371,21 @@ defmodule Number do
               %InterpreterTerms.SymbolMatch{
                 symbol: :NumericLiteralUnsigned,
                 submatches: [
-                  %InterpreterTerms.SymbolMatch{
-                    symbol: :DECIMAL,
-                    string: str,
-                    submatches: :none } ] } ] } ] }
+                  %InterpreterTerms.SymbolMatch{symbol: :DECIMAL, string: str, submatches: :none}
+                ]
+              }
+            ]
+          }
+        ]
+      }
     end
 
-    def to_sparql_result_value( %Number{ str: str } ) do
+    def to_sparql_result_value(%Number{str: str}) do
       # We do not know the type of the number, hence we output the
       # string as a decimal.  We should verify whether or not there
       # are formatting rules about this and/or whether we can import
       # the specific type from the definition.
-      %{ type: "literal", value: str, datatype: "http://www.w3.org/2001/XMLSchema#decimal" }
+      %{type: "literal", value: str, datatype: "http://www.w3.org/2001/XMLSchema#decimal"}
     end
   end
 end
-  
