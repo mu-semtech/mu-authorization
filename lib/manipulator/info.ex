86beebec60a3bb26271ca2c26ff3e0173dd58df3
Aad Versteden <madnificent@gmail.com>
2019-04-23 12:54:40 +0200

    Add overseen Manipulator.Info file
    Some previous functionality will be brocken as this file was lacking.

---
+++ lib/manipulator/info.ex
@@ -0,0 +1,18 @@
+defmodule Manipulators.Info do
+  require Manipulators.Basics
+
+  @moduledoc """
+  Helpers to extract common information from a query.
+  """
+
+  @doc """
+  Extracts prefixes and base from the supplied query and yields them as a map.
+  """
+  def prologue_map(query) do
+    Manipulators.Basics.do_state_map {%{}, query}, {state, elt} do
+      :Prologue ->
+        {:exit, Updates.QueryAnalyzer.import_prologue(elt, state), nil}
+    end
+    |> elem( 1 )
+  end
+end
