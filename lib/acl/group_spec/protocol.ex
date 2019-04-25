3084272ce52a300d70f6e479d6d85bfdab8373cb
Aad Versteden <madnificent@gmail.com>
2019-04-25 23:49:18 +0200

    Renaming Acl.GroupSpec.Protocol's user_group to matched_user_group
    The new name better explains what is meant here.  The indication
    seemed to be that real user groups would be returned here, where it is
    a user group's name with matching configuration parameters applicable
    in an individual case.

--- lib/acl/group_spec/protocol.ex
+++ lib/acl/group_spec/protocol.ex
@@ -5,32 +5,32 @@ defprotocol Acl.GroupSpec.Protocol do
   @type t :: struct()
   @type query :: struct()
   @type request :: struct()
-  @type user_group :: {String.t, [String.t]} # this shouldn't be named user_group but user_group_info or something of the likes.  It contains the user_group name and its corresponding variables.
+  @type matched_user_group :: {String.t, [String.t]}
 
   @doc """
   Indicates whether or not the given group_spec is accessible for the
   current user.  This item could be cached in the future based on the
   accessibility response.
   """
-  @spec accessible?( GPr.t, GPr.request ) :: { :ok, [GPr.user_group] } | false
+  @spec accessible?( GPr.t, GPr.request ) :: { :ok, [GPr.matched_user_group] } | false
   def accessible?( group_spec, request )
 
   @doc """
   Processing of a set of quads is hanlded by this method.  It
   transforms the supplied set of quads to a new set of quads.
   """
-  @spec process( GPr.t, GPr.user_group, [Quad] ) :: [Quad]
-  def process( group_spec, user_group, quads )
+  @spec process( GPr.t, GPr.matched_user_group, [Quad] ) :: [Quad]
+  def process( group_spec, matched_user_group, quads )
 
   @doc """
   Rewrites a SPARQL GET query so it matches the required access
   rights.  In practice, this boils down to rewriting the query so only
   allowed graphs will be queried.
 
-  The returned array of APr.user_group contains all of the user groups
+  The returned array of APr.matched_user_group contains all of the user groups
   which could have added content to the response.  An empty array
   means no response could be supplied by this processing.
   """
-  @spec process_query( GPr.t, GPr.user_group, GPr.query ) :: { GPr.query, [GPr.user_group] }
-  def process_query( group_spec, user_group, query )
+  @spec process_query( GPr.t, GPr.matched_user_group, GPr.query ) :: { GPr.query, [GPr.matched_user_group] }
+  def process_query( group_spec, matched_user_group, query )
 end
