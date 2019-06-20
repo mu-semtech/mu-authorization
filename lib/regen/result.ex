b99d4dc01e2ea5cf9f34073ea6e8e083cfce3a75
Aad Versteden <madnificent@gmail.com>
2019-06-20 10:49:01 +0200

    Make Regen.Result.as_sparql_p tail-recursive
    We notice some issues popping by in this call and hope tail-recursion
    may help limit the stack and push the calls through.  The issues don't
    appear on many machines but we want to use the situation to make the
    code better.

--- lib/regen/result.ex
+++ lib/regen/result.ex
@@ -10,37 +10,53 @@ defmodule Regen.Result do
   end
 
   def as_sparql(%Regen.Status{produced_content: arr}, options \\ %{}) do
-    as_sparql_p(Enum.reverse(arr), ensure_basic_options(options))
+    as_sparql_p("", Enum.reverse(arr), ensure_basic_options(options))
   end
 
-  defp as_sparql_p(["{" | rest], %{spaces: spaces} = options) do
-    "\n" <>
-      n_spaces(spaces) <>
-      "{" <>
-      "\n" <>
-      n_spaces(spaces + 2) <>
-      as_sparql_p(rest, %{options | spaces: spaces + 2})
+  defp as_sparql_p(base, ["{" | rest], %{spaces: spaces} = options) do
+    new_content =
+      base <>
+        "\n" <>
+        n_spaces(spaces) <>
+        "{" <>
+        "\n" <>
+        n_spaces(spaces + 2)
+
+    as_sparql_p(new_content, rest, %{options | spaces: spaces + 2})
   end
 
-  defp as_sparql_p(["}" | rest], %{spaces: spaces} = options) do
-    "\n" <>
-      n_spaces(spaces - 2) <>
-      "}" <>
-      "\n" <>
-      n_spaces(spaces - 2) <>
-      as_sparql_p(rest, %{options | spaces: spaces - 2})
+  defp as_sparql_p(base, ["}" | rest], %{spaces: spaces} = options) do
+    new_content =
+      base <>
+        "\n" <>
+        n_spaces(spaces - 2) <>
+        "}" <>
+        "\n" <>
+        n_spaces(spaces - 2)
+
+    as_sparql_p(new_content, rest, %{options | spaces: spaces - 2})
   end
 
-  defp as_sparql_p(["." | rest], %{spaces: spaces} = options) do
-    "." <> "\n" <> n_spaces(spaces) <> as_sparql_p(rest, options)
+  defp as_sparql_p(base, ["." | rest], %{spaces: spaces} = options) do
+    new_content =
+      base <>
+        "." <>
+        "\n" <>
+        n_spaces(spaces)
+
+    as_sparql_p(new_content, rest, options)
   end
 
-  defp as_sparql_p([word | rest], options) do
-    word <> " " <> as_sparql_p(rest, options)
+  defp as_sparql_p(base, [word | rest], options) do
+    new_content =
+      base <>
+        word <> " "
+
+    as_sparql_p(new_content, rest, options)
   end
 
-  defp as_sparql_p([], _) do
-    ""
+  defp as_sparql_p(base, [], _) do
+    base
   end
 
   defp ensure_basic_options(options) do
