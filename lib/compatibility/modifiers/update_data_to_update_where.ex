faf157f0c7947f0aa000a4a2af6c34fc07b3cc8a
Aad Versteden <madnificent@gmail.com>
2019-06-21 09:24:44 +0200

    Cleanup Virtuoso compat
    The Virtuoso compat layer has been cut into multiple pieces, making
    the higher level intention more clear.

--- lib/compatibility/modifiers/update_data_to_update_where.ex
+++ lib/compatibility/modifiers/update_data_to_update_where.ex
@@ -8,7 +8,27 @@ defmodule Compat.Modifiers.UpdateDataToUpdateWhere do
 
   @impl Compat.QueryManipulator
   def manipulate(query) do
-    delete_quad_data =
+    case extract_quad_data_and_quads_from_delete_data(query) do
+      {:error, _} ->
+        query
+
+      {:ok, quad_data, quads} ->
+        optionalized_group_graph_pattern =
+          quad_data
+          |> Manipulator.Transform.quad_data_to_group_graph_pattern()
+          |> make_graph_graph_patterns_optional()
+
+        delete_data_to_delete_where_query(query, quads, optionalized_group_graph_pattern)
+    end
+  end
+
+  defp extract_quad_data_and_quads_from_delete_data(query) do
+    # Extracts quads and data from a delete data query or yields
+    # {:error, _} tuple when not a delete data query.
+    #
+    # Returns { :ok, quad_data, quads } on success
+    # Returns { :erorr, message_or_query } on failure
+    query_detection =
       Manipulators.Basics.do_state_map {:no_delete, query}, {state, element} do
         %Sym{
           symbol: :Update1,
@@ -18,60 +38,31 @@ defmodule Compat.Modifiers.UpdateDataToUpdateWhere do
               submatches: [
                 %Word{},
                 %Word{},
-                %Sym{symbol: :QuadData} = quad_data
+                %Sym{
+                  symbol: :QuadData,
+                  submatches: [
+                    %Word{},
+                    quads,
+                    %Word{}
+                  ]
+                } = quad_data
               ]
             }
           ]
         } ->
-          {:exit, {:is_delete, quad_data}, nil}
+          {:exit, {:is_delete, quad_data, quads}, nil}
       end
 
-    case delete_quad_data do
-      {:no_delete, query} ->
-        IO.puts("Not a delete query")
-        query
-
-      {:exit, {:is_delete, quad_data}, nil} ->
-        IO.puts("A delete query!")
-        IO.puts("Delete quad data")
-        Regen.result(quad_data, :QuadData)
-
-        optionalized_group_graph_pattern =
-          quad_data
-          |> Manipulator.Transform.quad_data_to_group_graph_pattern()
-          |> make_graph_graph_patterns_optional()
-
-        %Sym{symbol: :QuadData, submatches: [%Word{}, quads, %Word{}]} = quad_data
-
-        Manipulators.Basics.do_map query, element do
-          %Sym{symbol: :DeleteData} ->
-            {:replace_by,
-             %Sym{
-               symbol: :Modify,
-               submatches: [
-                 %Sym{
-                   symbol: :DeleteClause,
-                   submatches: [
-                     %Word{word: "DELETE"},
-                     %Sym{
-                       symbol: :QuadPattern,
-                       submatches: [
-                         %Word{word: "{"},
-                         quads,
-                         %Word{word: "}"}
-                       ]
-                     }
-                   ]
-                 },
-                 %Word{word: "WHERE"},
-                 optionalized_group_graph_pattern
-               ]
-             }}
-        end
+    case query_detection do
+      {:no_delete, query} -> {:error, query}
+      {:exit, {:is_delete, quad_data, quads}, nil} -> {:ok, quad_data, quads}
     end
   end
 
   defp make_graph_graph_patterns_optional(query) do
+    # Makes all :GraphGraphPattern instances optional.  Assuming
+    # wrapping in graphs exists.  If none are found, nothing is
+    # converted.
     Manipulators.Basics.do_map query, element do
       %Sym{
         symbol: :GraphGraphPattern,
@@ -81,10 +72,6 @@ defmodule Compat.Modifiers.UpdateDataToUpdateWhere do
           group_graph_pattern
         ]
       } ->
-        IO.inspect(Regen.result(group_graph_pattern, :GroupGraphPattern),
-          label: "GroupGraphPattern used for updating"
-        )
-
         {:replace_by,
          %Sym{
            symbol: :GraphGraphPattern,
@@ -115,6 +102,37 @@ defmodule Compat.Modifiers.UpdateDataToUpdateWhere do
                  %Word{word: "}"}
                ]
              }
+           ]
+         }}
+    end
+  end
+
+  defp delete_data_to_delete_where_query(query, quads, group_graph_pattern) do
+    # Converts a :DeleteData query into a :DeleteWhere query, assuming
+    # the necessary bits to alter are supplied.  Assumes extraction of
+    # quads and conversion to group_graph_pattern has happened before.
+    Manipulators.Basics.do_map query, element do
+      %Sym{symbol: :DeleteData} ->
+        {:replace_by,
+         %Sym{
+           symbol: :Modify,
+           submatches: [
+             %Sym{
+               symbol: :DeleteClause,
+               submatches: [
+                 %Word{word: "DELETE"},
+                 %Sym{
+                   symbol: :QuadPattern,
+                   submatches: [
+                     %Word{word: "{"},
+                     quads,
+                     %Word{word: "}"}
+                   ]
+                 }
+               ]
+             },
+             %Word{word: "WHERE"},
+             group_graph_pattern
            ]
          }}
     end
