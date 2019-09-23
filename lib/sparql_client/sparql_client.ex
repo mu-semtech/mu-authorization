e2379e2c262d22e6db79eb69dd735c8ac80cba2f
Aad Versteden <madnificent@gmail.com>
2019-09-23 14:46:12 +0200

    Add exponential backoff to failing SPARQL queries
    When a SPARQL query fails in production, there's a fair chance the
    failure is coming from a backend error.  This is an approach to let us
    catch these errors and retry the call multiple times.
    
    It's not trivial to reach these errors in a simple development setup.
    This feature will be launched on a more complex stack to check what
    other errors pop up.

--- lib/sparql_client/sparql_client.ex
+++ lib/sparql_client/sparql_client.ex
@@ -37,7 +37,25 @@ defmodule SparqlClient do
 
     Logging.EnvLog.inspect(query, :inspect_outgoing_sparql_queries, label: "Outgoing SPARQL query")
 
-    query_info = InfoEndpoint.start_query( query )
+    query_info = InfoEndpoint.start_query(query)
+
+    do_execute_query({query, query_info, outgoing_headers, poison_options})
+  end
+
+  defp do_execute_query(query_spec), do: do_execute_query(query_spec, 10)
+
+  defp do_execute_query({query, query_info, _, _}, 0) do
+    Logger.error("Failed to execute query multiple times #{query}")
+    InfoEndpoint.end_query(query_info)
+    raise "Backend query retry limit reached"
+  end
+
+  defp do_execute_query({query, query_info, outgoing_headers, poison_options}, retries) do
+    timeout = query_timeout_for_call(retries)
+
+    if timeout > 0 do
+      Process.sleep(timeout)
+    end
 
     try do
       # form parameters
@@ -47,33 +65,57 @@ defmodule SparqlClient do
           default_endpoint(),
           [
             "query=" <>
-            URI.encode_www_form(query) <>
-            "&format=" <> URI.encode_www_form("application/sparql-results+json")
+              URI.encode_www_form(query) <>
+              "&format=" <> URI.encode_www_form("application/sparql-results+json")
           ],
           outgoing_headers,
           poison_options
         ).body
 
-      Logging.EnvLog.log(:log_outgoing_sparql_query_responses, "Response to sparql query: #{response}")
-      Logging.EnvLog.inspect(query, :inspect_outgoing_sparql_query_responses, label: "Response to sparql query:")
+      Logging.EnvLog.log(
+        :log_outgoing_sparql_query_responses,
+        "Response to sparql query: #{response}"
+      )
 
-      Logging.EnvLog.log(:log_outgoing_sparql_query_roundtrip, "Outgoing sparql query: #{query}\nincoming sparql response #{response}")
+      Logging.EnvLog.inspect(query, :inspect_outgoing_sparql_query_responses,
+        label: "Response to sparql query:"
+      )
+
+      Logging.EnvLog.log(
+        :log_outgoing_sparql_query_roundtrip,
+        "Outgoing sparql query: #{query}\nincoming sparql response #{response}"
+      )
 
       try do
-        InfoEndpoint.end_query( query_info )
-        Poison.decode!(response)
+        decoded_result = Poison.decode!(response)
+        InfoEndpoint.end_query(query_info)
+        decoded_result
       rescue
         exception ->
-          IO.inspect(response, label: "Response received from database")
-        # TODO when upgrading elixir, change to reraise
-        raise exception
+          Logger.warn("Failed to decode response from database")
+          IO.inspect(response, label: "Response which could not be decoded")
+          # TODO when upgrading elixir, change to reraise
+          raise exception
       end
     rescue
       exception ->
-        InfoEndpoint.end_query( query_info )
-        raise exception
+        Logger.warn("Failed to execute query #{query} on database")
+        IO.inspect(exception, label: "Execution thrown when executing query")
+        query_info = InfoEndpoint.retry_query(query_info)
+        do_execute_query({query, query_info, outgoing_headers, poison_options}, retries - 1)
     end
   end
+
+  defp query_timeout_for_call(10), do: 0
+  defp query_timeout_for_call(9), do: 10
+  defp query_timeout_for_call(8), do: 25
+  defp query_timeout_for_call(7), do: 100
+  defp query_timeout_for_call(6), do: 250
+  defp query_timeout_for_call(5), do: 500
+  defp query_timeout_for_call(4), do: 1_000
+  defp query_timeout_for_call(3), do: 3_000
+  defp query_timeout_for_call(2), do: 8_000
+  defp query_timeout_for_call(1), do: 20_000
 
   def execute_parsed(query, options \\ [])
 
