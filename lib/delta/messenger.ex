ba49d494b97649b7ba522c3344a2970876c42ca5
Aad Versteden <madnificent@gmail.com>
2022-02-17 13:25:19 +0100

    Add mu-session-id to delta messages
    Most cases shouldn't use the mu-session-id but rather use the
    mu-auth-allowed-groups, but there are situations where it's handy.
    Adding it.

--- lib/delta/messenger.ex
+++ lib/delta/messenger.ex
@@ -2,7 +2,7 @@ defmodule Delta.Messenger do
   require Logger
   require ALog
 
-  @type call_options :: [mu_call_id_trail: String.t()]
+  @type call_options :: [mu_call_id_trail: String.t(), mu_session_id: String.t()]
 
   @moduledoc """
   Sends constructed messages to all interested clients.
@@ -36,7 +36,8 @@ defmodule Delta.Messenger do
     headers = [
       {"Content-Type", "application/json"},
       {"mu-call-id", Integer.to_string(Enum.random(0..1_000_000_000_000))},
-      {"mu-call-id-trail", options[:mu_call_id_trail]}
+      {"mu-call-id-trail", options[:mu_call_id_trail]},
+      {"mu-session-id", options[:mu_session_id]}
     ]
 
     # we expect clients to respond to our request
