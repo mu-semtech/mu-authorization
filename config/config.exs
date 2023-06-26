2f30c2af931590b3d50f6a268fbcf00078f4abd4
Aad Versteden <madnificent@gmail.com>
2023-06-26 12:14:07 +0200

    Try to reinstall all deps and updating Config import

--- config/config.exs
+++ config/config.exs
@@ -1,44 +1,42 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
-defmodule CH do
-  def system_boolean(name, default \\ false) do
-    case String.downcase(System.get_env(name) || "") do
-      "true" -> true
-      "yes" -> true
-      "1" -> true
-      "on" -> true
-      "" -> default
-      _ -> false
-    end
+system_boolean = fn (name, default) ->
+  case String.downcase(System.get_env(name) || "") do
+    "true" -> true
+    "yes" -> true
+    "1" -> true
+    "on" -> true
+    "" -> default
+    _ -> false
   end
+end
 
-  def database_compatibility(name) do
+database_compatibility = fn (name) ->
+  (System.get_env(name) || "")
+  |> String.downcase()
+  |> case do
+       "virtuoso" -> Compat.Implementations.Virtuoso
+       _ -> Compat.Implementations.Raw
+     end
+end
+
+system_number = fn (name, default) ->
+  try do
     (System.get_env(name) || "")
-    |> String.downcase()
-    |> case do
-      "virtuoso" -> Compat.Implementations.Virtuoso
-      _ -> Compat.Implementations.Raw
-    end
+    |> String.to_integer()
+  rescue
+    ArgumentError -> default
   end
+end
 
-  def system_number(name, default) do
-    try do
-      (System.get_env(name) || "")
-      |> String.to_integer()
-    rescue
-      ArgumentError -> default
-    end
-  end
-
-  def system_float(name, default \\ false) do
-    try do
-      (System.get_env(name) || "")
-      |> String.to_float()
-    rescue
-      ArgumentError -> default
-    end
+system_float = fn (name, default) ->
+  try do
+    (System.get_env(name) || "")
+    |> String.to_float()
+  rescue
+    ArgumentError -> default
   end
 end
 
@@ -53,30 +51,29 @@ end
 #     config :sparqlex, key: :value
 config :"mu-authorization",
   author: :"mu-semtech",
-  log_server_configuration: CH.system_boolean("LOG_SERVER_CONFIGURATION"),
-  log_outgoing_sparql_queries: CH.system_boolean("LOG_OUTGOING_SPARQL_QUERIES"),
-  log_incoming_sparql_queries: CH.system_boolean("LOG_INCOMING_SPARQL_QUERIES"),
-  inspect_outgoing_sparql_queries: CH.system_boolean("INSPECT_OUTGOING_SPARQL_QUERIES"),
-  inspect_incoming_sparql_queries: CH.system_boolean("INSPECT_INCOMING_SPARQL_QUERIES"),
-  log_delta_messages: CH.system_boolean("LOG_DELTA_MESSAGES"),
-  log_template_matcher_performance: CH.system_boolean("LOG_TEMPLATE_MATCHER_PERFORMANCE"),
-  log_delta_client_communication: CH.system_boolean("LOG_DELTA_CLIENT_COMMUNICATION"),
-  log_access_rights: CH.system_boolean("LOG_ACCESS_RIGHTS"),
-  inspect_access_rights_processing: CH.system_boolean("INSPECT_ACCESS_RIGHTS_PROCESSING"),
-  database_compatibility: CH.database_compatibility("DATABASE_COMPATIBILITY"),
-  log_outgoing_sparql_query_responses: CH.system_boolean("LOG_OUTGOING_SPARQL_QUERY_RESPONSES"),
-  inspect_outgoing_sparql_query_responses:
-    CH.system_boolean("INSPECT_OUTGOING_SPARQL_QUERY_RESPONSES"),
-  log_outgoing_sparql_query_roundtrip: CH.system_boolean("LOG_OUTGOING_SPARQL_QUERY_ROUNDTRIP"),
+  log_server_configuration: system_boolean.("LOG_SERVER_CONFIGURATION", false),
+  log_outgoing_sparql_queries: system_boolean.("LOG_OUTGOING_SPARQL_QUERIES", false),
+  log_incoming_sparql_queries: system_boolean.("LOG_INCOMING_SPARQL_QUERIES", false),
+  inspect_outgoing_sparql_queries: system_boolean.("INSPECT_OUTGOING_SPARQL_QUERIES", false),
+  inspect_incoming_sparql_queries: system_boolean.("INSPECT_INCOMING_SPARQL_QUERIES", false),
+  log_delta_messages: system_boolean.("LOG_DELTA_MESSAGES", false),
+  log_template_matcher_performance: system_boolean.("LOG_TEMPLATE_MATCHER_PERFORMANCE", false),
+  log_delta_client_communication: system_boolean.("LOG_DELTA_CLIENT_COMMUNICATION", false),
+  log_access_rights: system_boolean.("LOG_ACCESS_RIGHTS", false),
+  inspect_access_rights_processing: system_boolean.("INSPECT_ACCESS_RIGHTS_PROCESSING", false),
+  database_compatibility: database_compatibility.("DATABASE_COMPATIBILITY"),
+  log_outgoing_sparql_query_responses: system_boolean.("LOG_OUTGOING_SPARQL_QUERY_RESPONSES", false),
+  inspect_outgoing_sparql_query_responses: system_boolean.("INSPECT_OUTGOING_SPARQL_QUERY_RESPONSES", false),
+  log_outgoing_sparql_query_roundtrip: system_boolean.("LOG_OUTGOING_SPARQL_QUERY_ROUNDTRIP", false),
   default_sparql_endpoint: System.get_env("MU_SPARQL_ENDPOINT") || "http://localhost:8890/sparql",
-  query_max_processing_time: CH.system_number("QUERY_MAX_PROCESSING_TIME", 120_000),
-  query_max_execution_time: CH.system_number("QUERY_MAX_EXECUTION_TIME", 60_000),
-  database_recovery_mode_enabled: CH.system_boolean("DATABASE_OVERLOAD_RECOVERY"),
-  log_database_recovery_mode_tick: CH.system_boolean("LOG_DATABASE_OVERLOAD_TICK"),
-  log_workload_info_requests: CH.system_boolean("LOG_WORKLOAD_INFO_REQUESTS"),
-  testing_auth_query_error_rate: CH.system_float("TESTING_AUTH_QUERY_ERROR_RATE"),
-  error_on_unwritten_data: CH.system_boolean("ERROR_ON_UNWRITTEN_DATA"),
-  errors: CH.system_boolean("LOG_ERRORS", true)
+  query_max_processing_time: system_number.("QUERY_MAX_PROCESSING_TIME", 120_000),
+  query_max_execution_time: system_number.("QUERY_MAX_EXECUTION_TIME", 60_000),
+  database_recovery_mode_enabled: system_boolean.("DATABASE_OVERLOAD_RECOVERY", false),
+  log_database_recovery_mode_tick: system_boolean.("LOG_DATABASE_OVERLOAD_TICK", false),
+  log_workload_info_requests: system_boolean.("LOG_WORKLOAD_INFO_REQUESTS", false),
+  testing_auth_query_error_rate: system_float.("TESTING_AUTH_QUERY_ERROR_RATE", false),
+  error_on_unwritten_data: system_boolean.("ERROR_ON_UNWRITTEN_DATA", false),
+  errors: system_boolean.("LOG_ERRORS", true)
 
 # and access this configuration in your application as:
 #
@@ -95,7 +92,7 @@ config :logger,
   compile_time_purge_level: :debug,
   level: :warn
 
-if Mix.env() == :test do
+if config_env() == :test do
   config :junit_formatter,
     report_dir: "/tmp/repo-example-test-results/exunit"
 end
@@ -106,6 +103,6 @@ end
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-#     import_config "#{Mix.env}.exs"
+#     import_config "#{config_env()}.exs"
 
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :"mu-authorization",
   sparql_port: 9980
